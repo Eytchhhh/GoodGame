b651b6fd837131de4c788b64d0a5e64b0cb82c6b
Harvey Gilboy <harveygilboy123@gmail.com>
2022-09-22 10:40:16 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Router, RouterModule, ROUTES, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ProductsComponent } from './components/products/products.component';
+
+const routes: Routes = [
+  { 
+    path: 'main', component: LandingPageComponent 
+  },
+  { 
+    path: 'aboutUs', component: AboutUsComponent 
+  },
+  { 
+    path: 'contactUs', component: ContactUsComponent 
+  },
+  { 
+    path: 'dashboard', component: DashboardComponent 
+  },
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ServiceOfferedComponent } from './components/service-offered/service-offered.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingPageComponent,
+    CarouselComponent,
+    ServiceOfferedComponent,
+    ProductsComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
